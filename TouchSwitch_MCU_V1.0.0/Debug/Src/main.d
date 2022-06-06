d69a023bf34a03b7befeb8340aaea7d02355fa69
dung <dungpk@lumi.vn>
2022-06-06 12:01:40 +0700

    Clean Code 6/6//2022

---
+++ TouchSwitch_MCU_V1.0.0/Debug/Src/main.d
@@ -0,0 +1,40 @@
+Src/main.o: ../Src/main.c \
+ C:/Users/Dung/Desktop/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/rtos/eventman.h \
+ C:/Users/Dung/Desktop/SDK_1.0.3_NUCLEO-F401RE/shared/Utilities/buff.h \
+ C:/Users/Dung/Desktop/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/button/button.h \
+ C:/Users/Dung/Desktop/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/button/eventbutton.h \
+ C:/Users/Dung/Desktop/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/led/led.h \
+ C:/Users/Dung/Desktop/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/rtos/timer.h \
+ C:/Users/Dung/Desktop/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/system_stm32f4xx.h \
+ C:/Users/Dung/Desktop/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/buzzer/melody.h \
+ C:/Users/Dung/Desktop/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/buzzer/buzzer.h \
+ C:/Users/Dung/Desktop/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/sensor/temhumsensor.h \
+ C:/Users/Dung/Desktop/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/ucglib/Ucglib.h \
+ C:/Users/Dung/Desktop/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/ucglib/ucg.h \
+ C:/Users/Dung/Desktop/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/sensor/lightsensor.h
+
+C:/Users/Dung/Desktop/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/rtos/eventman.h:
+
+C:/Users/Dung/Desktop/SDK_1.0.3_NUCLEO-F401RE/shared/Utilities/buff.h:
+
+C:/Users/Dung/Desktop/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/button/button.h:
+
+C:/Users/Dung/Desktop/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/button/eventbutton.h:
+
+C:/Users/Dung/Desktop/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/led/led.h:
+
+C:/Users/Dung/Desktop/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/rtos/timer.h:
+
+C:/Users/Dung/Desktop/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/system_stm32f4xx.h:
+
+C:/Users/Dung/Desktop/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/buzzer/melody.h:
+
+C:/Users/Dung/Desktop/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/buzzer/buzzer.h:
+
+C:/Users/Dung/Desktop/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/sensor/temhumsensor.h:
+
+C:/Users/Dung/Desktop/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/ucglib/Ucglib.h:
+
+C:/Users/Dung/Desktop/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/ucglib/ucg.h:
+
+C:/Users/Dung/Desktop/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/sensor/lightsensor.h:
